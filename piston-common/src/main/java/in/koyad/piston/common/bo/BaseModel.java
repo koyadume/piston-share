d9b8fccf8a88b88c2b61dea8f2f362b243592d7f
Shailendra Singh <shailendra_01@outlook.com>
2019-02-24 10:00:39 +0530

    Added base model class.

---
+++ piston-common/src/main/java/in/koyad/piston/common/bo/BaseModel.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2012-2017 Shailendra Singh <shailendra_01@outlook.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package in.koyad.piston.common.bo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(Include.NON_NULL)
+@Getter 
+@Setter
+public abstract class BaseModel {
+
+	private String id;
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof BaseModel) {
+			return id.equals(((BaseModel)obj).id);
+		}
+		return false;
+	}
+	
+}
